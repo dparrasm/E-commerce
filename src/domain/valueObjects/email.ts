8a82e12c03556fe0b5f3789d983e1063751e77a3
dpiperai <david@piperai.com>
2025-02-19 22:56:53 +0100

    Creating and organising the first layers

---
+++ src/domain/valueObjects/email.ts
@@ -0,0 +1,18 @@
+export class Email {
+    private readonly value: string
+
+    constructor(email: string) {
+        if (!this.isValidEmail(email)) {
+            throw new Error("Invalid email format")
+        }
+        this.value = email
+    }
+
+    private isValidEmail(email: string): boolean {
+        return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)
+    }
+
+    public getValue(): string {
+        return this.value
+    }
+}
