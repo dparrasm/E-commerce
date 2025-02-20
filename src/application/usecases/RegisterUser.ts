165c61254aea7ed7dc2211e6b1fbec42f6b4822b
dpiperai <david@piperai.com>
2025-02-20 10:09:26 +0100

    first use case implemented

---
+++ src/application/usecases/RegisterUser.ts
@@ -0,0 +1,23 @@
+import { User } from './../../domain/entities/User';
+import { Email } from '../../domain/valueObjects/email';
+import { IUserRepository } from './../../domain/repositories/IUserRepository';
+import { v4 as uuidv4 } from 'uuid';
+
+export class RegisterUser {
+    constructor(private userRepository: IUserRepository) {}
+
+    async execute(user: User): Promise<User>{
+        const {email, name, password} = user
+        const emailV0 = new Email(email)
+
+        const existingUser = await this.userRepository.findByEmail(emailV0)
+
+        if(existingUser){
+            throw new Error("User with this email already exist")
+        }
+
+        const userV0: User = {id: uuidv4(), name, email: emailV0.getValue(), password}
+        await this.userRepository.save(userV0)
+        return userV0
+    }
+}
--- src/domain/repositories/IUserRepository.ts
+++ src/domain/repositories/IUserRepository.ts
@@ -1,6 +1,7 @@
 import { User } from "../entities/User"
+import { Email } from "../valueObjects/email"
 export interface IUserRepository {
     findById(id: string): Promise<User | null>
-    findByEmail(email: string): Promise<User | null>
+    findByEmail(email: Email): Promise<User | null>
     save(user: User): Promise<void>
 }
